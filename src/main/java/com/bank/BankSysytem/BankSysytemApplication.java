8f45dab44264161c865f6799d1182b2181871690
Anfrni <anferneerod@gmail.com>
2021-08-29 23:25:24 -0400

    initial commit

---
+++ src/main/java/com/bank/BankSysytem/BankSysytemApplication.java
@@ -0,0 +1,13 @@
+package com.bank.BankSysytem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSysytemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSysytemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/BankSysytem/BankSysytemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.BankSysytem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSysytemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
