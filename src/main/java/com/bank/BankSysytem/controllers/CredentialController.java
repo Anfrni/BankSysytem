ae81ce463a510a6986d354eb04b2fc98cd3f4ae3
Anfrni <anferneerod@gmail.com>
2021-10-17 17:17:15 -0400

    add javadoc for credentials implementation

--- src/main/java/com/bank/BankSysytem/controllers/CredentialController.java
+++ src/main/java/com/bank/BankSysytem/controllers/CredentialController.java
@@ -10,30 +10,56 @@ public class CredentialController {
 	private CredentialsService credServ;
 	//TODO CRUD operations
 	
+	/**
+	 * @param creds
+	 * 
+	 * add record to credentials database table
+	 */
 	public void createCredentials(Credentials creds) {
-		// add record to credentials database table
 		credServ.createCredentials(creds);
 	}
 	
+	/**
+	 * @param id
+	 * @return return credentials in database table associated with given id
+	 */
 	public Credentials getCredentials(Integer id) {
-		// list all records in credentaials database table
+		// 
 		return credServ.readCredentials(id);
 	}
 	
+	/**
+	 * @return list of all records in credentials database table
+	 */
 	public List<Credentials> getAllCredentials() {
-		// list all credentials in Credentials database table
 		return credServ.listCredentials();
 	}
 	
+	/**
+	 * @param id
+	 * @param password
+	 * 
+	 * update password of record that matches ID
+	 */
 	public void updatePassword(Integer id, String password) {
 		//TODO update password of record that matches ID
 	}
 
+	/**
+	 * @param id
+	 * 
+	 * delete record in credentials database that matches ID
+	 */
 	public void deleteCredentials(Integer id) {
-		// delete record in credentials database that matches ID
 		credServ.deleteCredentials(id);
 	}
 	
+	/**
+	 * @param id
+	 * @param newrole
+	 * 
+	 * update role of record with matching id to newrole
+	 */
 	public void promoteRole(Integer id, Integer newrole) {
 		//TODO update role of record with matching id to newrole
 	}
